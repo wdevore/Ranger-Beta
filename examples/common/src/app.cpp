8d777498edc71f4e749c0abfc9d8db8cab88aa52
wdevore <william.devore@gmail.com>
2025-04-21 22:04:28 -0400

    bump up version requirement to 4.1

--- examples/common/src/app.cpp
+++ examples/common/src/app.cpp
@@ -26,9 +26,10 @@ namespace Game
 
     bool App::createWindow(const std::string &title)
     {
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
         glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
         // Create a GLFWwindow object that we can use for GLFW's functions
